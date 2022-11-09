fecb3ba5f50cdf91dead01dfa35dc23e76a978d7
LIYiquan <yli32258@gmail.com>
2022-11-09 17:17:15 +0800

    init

--- src/Jungle/Controller/GlobalController.java
+++ src/Jungle/Controller/GlobalController.java
@@ -15,6 +15,8 @@ public class GlobalController {
      * page, manual page and the welcome page. The game will be started
      * after the user switch to game page.
      *
+     *
+     *
      */
     public void startGlobalController()
     {
